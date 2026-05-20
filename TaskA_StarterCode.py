d39bdad011314c7c35afbb98b775bd0f74eafc12
dev-dany <dhanish.ev@gmail.com>
2026-05-20 12:04:24 +0700

    Add files via upload

---
+++ TaskA_StarterCode.py
@@ -0,0 +1,22 @@
+
+rooms = [
+    ["Maple Hall", "MH101", "EMPTY", True],
+["Maple Hall", "MH204", "Jack Stevens", False],
+["Maple Hall", "MH112", "EMPTY", True],
+["Maple Hall", "MH122", "Mollie Fisher", True],
+["Maple Hall", "MH225", "EMPTY", False],
+["Maple Hall", "MH106", "EMPTY", True],
+["Maple Hall", "MH322", "Balteg Singh", True],
+["Oak Hall", "OH202", "Alison Bates", False],
+["Oak Hall", "OH101", "EMPTY", True],
+["Oak Hall", "OH202", "EMPTY", False],
+["Oak Hall", "OH128", "Moisa Khanouf", True],
+["Birch Hall", "BH128", "EMPTY", True],
+["Birch Hall", "BH127", "Edward Williams", True],
+["Birch Hall", "BH126", "John Williams", True],
+["Birch Hall", "BH408", "EMPTY", False],
+["Birch Hall", "BH402", "EMPTY", False],
+["Birch Hall", "BH417", "Priti Pei", False]
+
+]
+
